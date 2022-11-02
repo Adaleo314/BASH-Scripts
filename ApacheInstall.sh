c76ddd38b61e23c7e4d2b2f9fe75cb81a722efd9
Adam Leonard <115667156+Adaleo314@users.noreply.github.com>
2022-11-02 15:27:36 -0400

    Update ApacheInstall.sh

--- ApacheInstall.sh
+++ ApacheInstall.sh
@@ -8,3 +8,7 @@ sudo systemctl start httpd
 sudo systemctl enable httpd
 
 echo "<h1>Level Up Black Team is the Best Team! All the rest are just Forks & Clones! </h1>" > /var/www/html/index.html
+
+echo "<img src="https://nostalgicreads.com/wp-content/uploads/2021/07/Chuck-Norris-36_.jpg" width="800" height="495" alt="The Chuck Norris">" >> /var/www/html/index.html
+
+#Script needs to be run with sudo                                           
